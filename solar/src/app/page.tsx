54b97a57774d6503fc1ff005370fce2bb9f99803
yulia <langedoc@gmail.com>
2024-05-07 12:17:16 +0200

    feat: working on redux

--- solar/src/app/page.tsx
+++ solar/src/app/page.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store/store';
-
+import { ChakraProvider } from '@chakra-ui/react';
+import App from './app';
 
 export default function Home() {
   return (
     <Provider store={store}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </Provider>
   )
-    
 }
