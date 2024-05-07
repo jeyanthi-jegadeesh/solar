09a7f6052016e098f5a400986759d3c139162051
yulia <langedoc@gmail.com>
2024-05-07 12:09:40 +0200

    feat: working on redux

---
+++ solar/src/app/store/store.tsx
@@ -0,0 +1,17 @@
+import { combineReducers, applyMiddleware } from 'redux';
+import { configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk'; // asynch actions
+
+const rootReducer = combineReducers({
+});
+
+const middleware = [thunk];
+
+export const store = configureStore({
+  reducer: {}
+});
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
