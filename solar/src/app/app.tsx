f455f67f1c884d7b5e31a7bf0e3ce5bf74c365c8
Jeyanthi Jegadeesh <jeyanthi.s.jegadeesh@gmail.com>
2024-05-07 13:17:40 +0200

    feat: overlay changes

--- solar/src/app/app.tsx
+++ solar/src/app/app.tsx
@@ -1,4 +1,20 @@
+import Overlay from '@/components/OverlayComponent';
+import { Box} from '@chakra-ui/react';
 
 export default function App () {
-
+    return(
+        <div>
+         <Box
+            position="relative"
+            zIndex={0}
+            w="100vw" 
+            h="100vh" 
+            bgSize="cover" 
+            bgPosition="center" 
+            bgRepeat="no-repeat" 
+            style={{ backgroundImage: "url('/solar.png')" }} >   
+         </Box>
+         <Overlay></Overlay> 
+      </div>
+    );
 }
