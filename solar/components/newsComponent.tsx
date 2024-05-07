95650199bf1ec8d50cd73ef5651fc02fa7f04e13
yulia <langedoc@gmail.com>
2024-05-07 13:02:05 +0200

    feat: NewsComponent created

---
+++ solar/components/newsComponent.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../src/app/store/store';
+import { News } from '../src/app/store/newsSlice';
+import { Flex, Heading, Text } from '@chakra-ui/react'
+
+const NewsSection: React.FC = () => {
+  const newsState = useSelector((state: RootState) => state.news);
+  const {news} = newsState; // extracted the news array from the newsState object returned by useSelector
+
+  return (
+    <Flex height='100vp' alignItems='center' justifyContent='center' bgGradient='linear(to-t, blue.700, black)' color='white'>
+      {news.map((item: News) => (
+        <Flex direction='column' key={item.id}>
+          <Heading>{item.title}</Heading>
+          <Text>{item.text}</Text>
+        </Flex>
+      ))}
+    </Flex>
+  );
+}
+
+export default NewsSection;
