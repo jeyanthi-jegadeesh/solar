0a05301528206eb9ccf9af1de9ab86f6123b6909
yulia <langedoc@gmail.com>
2024-05-08 17:44:21 +0200

    working on planet info component

--- solar/src/components/OverlayPlanets.tsx
+++ solar/src/components/OverlayPlanets.tsx
@@ -1,12 +1,13 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import PlanetTimeline from "./Timeline";
+import PlanetsInfo from "./PlanetsInfo";
 
 const OverlayPlanets: React.FC = () => {
 
   return (
     <Box bg='green' position='fixed' top='0' right='0' bottom='0' width='50%' zIndex={10} m={5} p={10}>
       <PlanetTimeline />
-      <Text>Planets info here</Text>
+      <PlanetsInfo />
     </Box>
   )
 }
---
+++ solar/src/components/PlanetsInfo.tsx
@@ -0,0 +1,16 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
+const PlanetsInfo: React.FC = () => {
+
+  return (
+    <Box bg='blue'>
+      <Heading>Planet name</Heading>
+      <Text>Images carousel</Text>
+      <Text>Basic info</Text>
+      <Text>Short description</Text>
+      <Text>Articles carousel</Text>
+    </Box>
+  )
+}
+  
+export default PlanetsInfo;
