fc7af09b58b11fad79cc1c98256a323e439a383a
tmnbln <smssms@wp.pl>
2024-05-09 19:55:41 +0200

    planetsinfo del

--- solar/src/app/app.tsx
+++ solar/src/app/app.tsx
@@ -1,5 +1,4 @@
 import OverlayLanding from '@/components/OverlayLandingComponent';
-import PlanetsInfo from '@/components/PlanetsInfo';
 import SpaceExplorer from '@/components/SpaceExplorer/SpaceExplorer';
 import { Box} from '@chakra-ui/react';
 import OverlayDialog from '@/components/OverlayDialog';
