c3bd676cc0d8656369ae55466923a38a4ed99d5d
Sathesh Ram Karunanithy <satheshram6@gmail.com>
2024-05-10 10:13:12 +0100

    Added dark mode icon

--- solar/src/components/Navbar.tsx
+++ solar/src/components/Navbar.tsx
@@ -1,34 +1,64 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
 import { showOverlay, hideOverlay } from '../app/store/overlaySlice';
-import { Link, Icon } from '@chakra-ui/react';
-import { FiUser } from 'react-icons/fi'; 
+import { Link, Flex, IconButton, Icon, Box, useColorMode, ChakraProvider } from '@chakra-ui/react';
+import { FiUser, FiSun, FiMoon } from 'react-icons/fi';
 
 const Navbar: React.FC = () => {
     const dispatch = useDispatch();
+    const { colorMode, toggleColorMode } = useColorMode(); //Chakra's useColorMode hook
+
     const handleShowOverlay = () => {
         dispatch(showOverlay());
     };
+
     const handleHideOverlay = () => {
         dispatch(hideOverlay());
     };
 
+    const handleQuizClick = () => {
+        //quiz button click logic here
+    };
+
+    const handleToggleColorMode = () => {
+        console.log('Current color mode:', colorMode);
+        toggleColorMode();
+        console.log('Color mode after toggle:', colorMode);
+    };
+    
+
     return (
         <nav className="navbar">
-            <ul className="nav-list">
-                <li className="nav-item">
-                    <Link href="#" fontSize="lg" fontWeight="bold" color="white" onClick={handleShowOverlay}>
-                        Home
-                    </Link>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" onClick={handleHideOverlay}>Explorer</a>
-                </li>
-                <li className="nav-item">
-                    {/* I am displaying a user icon for Profile */}
-                    <Icon as={FiUser} color="white" boxSize={20} />
-                </li>
-            </ul>
+            <Flex align="center">
+                <ul className="nav-list" style={{ marginRight: 'auto' }}>
+                    <li className="nav-item">
+                        <Link href="#" fontSize="lg" fontWeight="bold" color="white" onClick={handleShowOverlay}>
+                            Home
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="#" fontSize="lg" fontWeight="bold" color="white" onClick={handleHideOverlay}>
+                            Explore
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link href="#" fontSize="lg" fontWeight="bold" color="white" onClick={handleQuizClick}>
+                            Quiz
+                        </Link>
+                    </li>
+                </ul>
+                <Box mr={20}>
+                    <IconButton
+                        aria-label="Toggle dark mode"
+                        icon={colorMode === 'dark' ? <Icon as={FiSun} /> : <Icon as={FiMoon} />}
+                        onClick={toggleColorMode}
+                        color="white"
+                        variant="ghost"
+                        size="sm"
+                    />
+                </Box>
+                <Icon as={FiUser} color="white" boxSize={20} />
+            </Flex>
         </nav>
     );
 };
