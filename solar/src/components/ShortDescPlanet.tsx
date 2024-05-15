9f0ab711c380ce12f41d158c03403fc98beda4a4
yulia <langedoc@gmail.com>
2024-05-15 23:03:15 +0200

    fix: fetchDescription warning fixed

--- solar/src/components/ShortDescPlanet.tsx
+++ solar/src/components/ShortDescPlanet.tsx
@@ -7,20 +7,20 @@ const ShortDescPlanet = () => {
   const [description, setDescription] = useState('');
   const planet = useSelector((state: RootState) => state.solarSystem.selectedPlanet);
 
-  const fetchDescription = async() => {
-    try {
-      const response = await fetch('/api/planetDescription/'+planet);
-      console.log(response);
-      const planetDescription = await response.text();
-      setDescription(planetDescription);
-    } catch (error) {
-      console.log('Errorfetching description', error);
-    }
-  }
-
+  
   useEffect(() => {
+    const fetchDescription = async() => {
+      try {
+        const response = await fetch('/api/planetDescription/'+planet);
+        console.log(response);
+        const planetDescription = await response.text();
+        setDescription(planetDescription);
+      } catch (error) {
+        console.log('Errorfetching description', error);
+      }
+    }
     fetchDescription()
-  },[]);
+  },[planet]);
 
   return (
     <Box height='100%' overflow='auto' bg='blue.50' color='blue.900' border='2px' borderColor='blue.900' borderRadius='5' p='5' dangerouslySetInnerHTML={{ __html: description }} /> 
